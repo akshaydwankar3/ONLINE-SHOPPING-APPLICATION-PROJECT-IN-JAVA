acd85ea736bd3b762cf40d7199285418e1c39068
Lrssrrb <sisodiyarana07@gmail.com>
2022-06-04 11:05:18 +0530

    Address added

---
+++ OnlineShoppingApplication/src/main/java/com/masai/model/Address.java
@@ -0,0 +1,31 @@
+package com.masai.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Entity
+@Data
+@AllArgsConstructor
+public class Address {
+	
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
+	private String city;
+	private String state;
+	private String pincode;
+	public Address(String city, String state, String pincode) {
+		super();
+		this.city = city;
+		this.state = state;
+		this.pincode = pincode;
+	}
+	
+	
+}
---
+++ OnlineShoppingApplication/src/main/java/com/masai/model/dxnxn.java
@@ -0,0 +1,12 @@
+package com.masai.model;
+
+public class dxnxn {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Address add = new Address("gy","hj","hj");
+		System.out.println(add);
+	}
+
+}
