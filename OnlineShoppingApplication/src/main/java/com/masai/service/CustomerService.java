f73b66cf10398fcc0e3e39f9ae0930760dca249d
zaki-ansari <99637633+zaki-ansari@users.noreply.github.com>
2022-06-07 11:34:38 +0530

    service-customer

---
+++ OnlineShoppingApplication/src/main/java/com/masai/service/CustomerService.java
@@ -0,0 +1,21 @@
+package com.masai.service;
+
+import java.util.List;
+
+import com.masai.exception.CustomerException;
+import com.masai.model.Customer;
+
+public interface CustomerService {
+	
+
+	public Customer addCustomer(Customer customer);
+	
+	public Customer updateCustomer(Customer customer)throws CustomerException;
+	
+	public Customer removeCustomer(Integer customerId)throws CustomerException;
+	
+	public Customer viewCustomer(Integer customerId)throws CustomerException;
+	
+	public List<Customer> viewAllCustomers()throws CustomerException;
+
+}
