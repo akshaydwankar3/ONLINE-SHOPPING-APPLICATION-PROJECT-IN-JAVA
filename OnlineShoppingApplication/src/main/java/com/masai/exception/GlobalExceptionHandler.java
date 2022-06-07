df58bf51c1d6e2e3e90be3b16c39783dc12942b6
Zaki Ansari <99637633+zaki-ansari@users.noreply.github.com>
2022-06-07 13:37:01 +0530

    customer-exception

--- OnlineShoppingApplication/src/main/java/com/masai/exception/GlobalExceptionHandler.java
+++ OnlineShoppingApplication/src/main/java/com/masai/exception/GlobalExceptionHandler.java
@@ -12,30 +12,40 @@ import org.springframework.web.servlet.NoHandlerFoundException;
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
-	@ExceptionHandler(ProductNotFoundException.class)
-	public ResponseEntity<MyError> myExceptionHandler(ProductNotFoundException pne, WebRequest wr){
-		MyError err= new MyError(LocalDateTime.now(),pne.getMessage(), wr.getDescription(false));
-		return new ResponseEntity<MyError>(err, HttpStatus.BAD_REQUEST);
+	@ExceptionHandler(CustomerException.class)
+	public ResponseEntity<MyErrorDetails> myExpHandler(CustomerException ie,WebRequest wr)  {
+		System.out.println("inside myHandler method...RNE");
+		
+		MyErrorDetails err = new MyErrorDetails(LocalDateTime.now(), ie.getMessage(), wr.getDescription(false));
+		
+		
+	
+	 return new ResponseEntity<MyErrorDetails>(err, HttpStatus.BAD_REQUEST);		
+		
 	}
 	
-	@ExceptionHandler(OrderNotFoundException.class)
-	public ResponseEntity<MyError> orderExceptionHandler(OrderNotFoundException pne, WebRequest wr){
-		MyError err= new MyError(LocalDateTime.now(),pne.getMessage(), wr.getDescription(false));
-		return new ResponseEntity<MyError>(err, HttpStatus.BAD_REQUEST);
-	}
+	
 	
 	@ExceptionHandler(NoHandlerFoundException.class)
-	public ResponseEntity<MyError> mynotFoundHandler(NoHandlerFoundException nfe,WebRequest req)  {
-		
-		MyError err=new MyError(LocalDateTime.now(), nfe.getMessage(), req.getDescription(false));
+	public ResponseEntity<MyErrorDetails> mynotFoundHandler(NoHandlerFoundException nfe,WebRequest req)  {
+		System.out.println("inside myHandler method...NHFE");
+
+		MyErrorDetails err=new MyErrorDetails(LocalDateTime.now(), nfe.getMessage(), req.getDescription(false));
 
 	return new ResponseEntity<>(err,HttpStatus.NOT_FOUND);
 				
 }
-	
-	public ResponseEntity<MyError> myExcHandler(Exception e, WebRequest wr){
-		MyError err=new MyError(LocalDateTime.now(),e.getMessage(), wr.getDescription(false));
+	@ExceptionHandler(Exception.class)
+	public ResponseEntity<MyErrorDetails> myExpHandlerMain(Exception ie,WebRequest wr)  {
+		System.out.println("inside myHandler method...EXP");
 		
-		return new ResponseEntity<MyError>(err, HttpStatus.BAD_REQUEST);
+
+		MyErrorDetails err = new MyErrorDetails(LocalDateTime.now(), ie.getMessage(), wr.getDescription(false));
+				
+	
+	 return new ResponseEntity<MyErrorDetails>(err, HttpStatus.BAD_REQUEST);		
+				
 	}
+	
+	
 }
